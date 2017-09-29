44be93907421ece512cf59241fc442232fffdfe7
Stanley <sjacob7@utexas.edu>
2017-09-29 17:36:32 -0500

    Revised lambda function and main function

--- char_star_test.cc
+++ char_star_test.cc
@@ -5,12 +5,12 @@ class MockClass {
 public:
 	template <class Comp>
 	int doSomething(int a, int b, Comp lambdaFunc) {
-		return lambdaFunc(a, b, addedValue);
+		return lambdaFunc(a, b);
 	}
 
 	int addSetStuff(int a, int b) {
-		auto lambdaFuncAdd = [](int a, int b, int addedValue) {
-			return a + b + addedValue;
+		auto lambdaFuncAdd = [this](int a, int b) {
+			return a + b + this->addedValue;
 		};
 		int result = doSomething(a, b, lambdaFuncAdd);
 		return result;
@@ -20,12 +20,17 @@ public:
 };
 
 int main() {
-	const char* a = "hiasdf;lerkasdf;lks;dkfljkl;kkfkfksdlf";
-	int size = strlen(a);
+	const char* a = "hiasdf";
+	int size = strlen(a) + 1
+	;
+	printf("%d\n", size);
 	char b[size];
-	strcpy(b, a);
-	printf("%s\n", b);
 
+	strcpy(b, a);
+	for (int i = 0; i < (size + 1); ++i) {
+		printf("%c\n", b[i]);
+	}
+	
 	MockClass temp;
 	printf("%d\n", temp.addSetStuff(4, 5));
 
